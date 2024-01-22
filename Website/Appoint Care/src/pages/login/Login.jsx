c18f75a0061e4776c6efd08082fd62431b654888
Lance Kian <lafa.flores.up@phinmaed.com>
2024-01-22 08:20:30 +0800

    Current Website

---
+++ Website/Appoint Care/src/pages/login/Login.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import WelcomeDesign from '../../components/reusecomponent/WelcomeDesign'
+import './Login.css'
+
+const Login = () => {
+    return (
+        <section className='login-section'>
+            <div className='container flex-column text-center align-item-center justify-content-center'>
+                <div className="container">
+                    <WelcomeDesign />
+                    <hr />
+                    <div className='signup flex-column justify-content-center align-items-center text-center'>
+                        <div className="row">
+                            <div className="col-4">
+                            </div>
+                            <div className="col-4">
+                                <form action="" className='login-form mx-auto'>
+                                    <h2 style={{ color: '#6EAB36' }}>Log In</h2>
+                                    <h4>Please complete the following details to proceed</h4>
+                                    <label className="d-block" htmlFor="">First Name *</label>
+                                    <input type="text" placeholder="Enter your First name" />
+                                    <label className="d-block" htmlFor="">Middle Name *</label>
+                                    <input type="text" placeholder="Enter your Middle name" />
+                                    <label className="d-block" htmlFor="">Last Name *</label>
+                                    <input type="text" placeholder="Enter your Last Name" />
+                                    <label htmlFor="datepicker" className="d-block">Select a Date:</label>
+                                    <input type="date" id="datepicker" name="datepicker"></input>
+                                    <label className="d-block" htmlFor="">Email *</label>
+                                    <input type="text" placeholder="Enter your Email" />
+                                    <button className=" d-block mx-auto submit-login">Submit</button>
+                                </form>
+                            </div>
+                            <div className="col-4">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Login
